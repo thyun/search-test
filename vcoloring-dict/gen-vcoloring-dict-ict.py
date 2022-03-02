459c40477e7249e82525061da05dca774bb23f9d
TaeHyoung Yun <th.yun@sk.com>
2022-03-02 18:56:29 +0900

    유사어 생성/수정 script 추가 - admin api 호출 방식

--- vcoloring-dict/gen-vcoloring-dict-ict.py
+++ vcoloring-dict/gen-vcoloring-dict-ict.py
@@ -10,9 +10,35 @@ from nltk.corpus import words
 # import nltk
 # nltk.download('words')
 
-API_HOST = "pri-api.dev.srch.skplanetx.com"
+API_HOST = "pri-admin.dev.srch.skplanetx.com"
 API_PORT = 80
-API_PATH = "/api/common/analyze/tokenizer"
+API_UPSERT_PATH = "/apis/v2/dictionary/upsert"
+
+def call_api(keyword, value):
+    conn = http.client.HTTPConnection(API_HOST, API_PORT)
+    url = API_UPSERT_PATH
+
+    headers = {'Content-type': 'application/json'}
+    body = {
+  "chnl": "vcoloring",
+  "requestData": {
+    "channelId": 4,
+    "type": 2,
+    "id": 0,
+    "keyword": keyword,
+    "value": value,
+    "delimiter": "",
+    "useFlag": "1",
+    "userId": "1001291"
+  }
+}
+    json_data = json.dumps(body)
+    print("json_data=", json_data)
+
+    conn.request("POST", url, json_data, headers)
+    response = conn.getresponse()
+    rsbody = response.read()
+    print(response.status, rsbody)
 
 def process_csv_userdict(file_path):
     noun_dict = { }
@@ -41,16 +67,17 @@ def process_csv_synonym(file_path):
             value = row['유의어']
             print(f"{keyword} => {value}")
 
-def process_csv_synonym_keyword(file_path):
+def process_csv_synonym2(file_path):
     noun_dict = { }
     print("process_csv_synonym_keyword() start: {} {}".format(file_path, datetime.today()))
     with open(file_path) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             keyword = row['검색어']
-            value = row['유의어']
-            if ("," in keyword):
-                print(f"{keyword} => {value}")
+            value = row['유사어']
+            print(f"{keyword} => {value}")
 
-process_csv_synonym_keyword("rmc-ict-synonym-20211206.csv")
+#process_csv_synonym("rmc-ict-synonym-20211206.csv")
+#process_csv_synonym2("rmc-ict-synonym-create-202202.csv")
+call_api("aaa1", "aaa1")
 
