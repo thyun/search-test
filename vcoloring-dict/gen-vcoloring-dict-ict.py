18a418834ab51bc2fd35c6863a5fdc17ebcfe1bc
TaeHyoung Yun <th.yun@sk.com>
2022-03-04 16:54:15 +0900

    유사어 생성/수정/삭제 script 추가 - csv 파일 처리

--- vcoloring-dict/gen-vcoloring-dict-ict.py
+++ vcoloring-dict/gen-vcoloring-dict-ict.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 사업팀 생성 사전 데이터 처리
 import json
 import re
@@ -10,13 +11,14 @@ from nltk.corpus import words
 # import nltk
 # nltk.download('words')
 
-API_HOST = "pri-admin.dev.srch.skplanetx.com"
+API_HOST = "pri-admin.srch.skplanetx.com" # 상용
+#API_HOST = "pri-admin.dev.srch.skplanetx.com" # 개발
 API_PORT = 80
-API_UPSERT_PATH = "/apis/v2/dictionary/upsert"
+API_DICT_PATH = "/apis/v2/dictionary"
 
-def call_api(keyword, value):
+def call_api_list(keyword):
     conn = http.client.HTTPConnection(API_HOST, API_PORT)
-    url = API_UPSERT_PATH
+    url = API_DICT_PATH + "/list"
 
     headers = {'Content-type': 'application/json'}
     body = {
@@ -24,7 +26,38 @@ def call_api(keyword, value):
   "requestData": {
     "channelId": 4,
     "type": 2,
-    "id": 0,
+    "keyword": keyword,
+    "page": 0,
+    "size": 10
+  }
+}
+    json_data = json.dumps(body)
+    conn.request("POST", url, json_data, headers)
+    response = conn.getresponse()
+    rsbody = response.read()
+    jo = json.loads(rsbody)
+    responseData = jo["responseData"]
+    print("call_api_list(): keyword=", keyword, 
+        "status=", response.status, "resCode1=", jo["resCode1"], "resCode2=", jo["resCode2"], "detailMsg=", jo["detailMsg"],
+        "responseData.list=", responseData["list"])
+
+    totalCnt = responseData["totalCnt"]
+    if (totalCnt == 1):
+        row = responseData["list"][0]
+        return row["id"]
+    return 0
+
+def call_api_upsert(keyword, value, id):
+    conn = http.client.HTTPConnection(API_HOST, API_PORT)
+    url = API_DICT_PATH + "/upsert"
+
+    headers = {'Content-type': 'application/json'}
+    body = {
+  "chnl": "vcoloring",
+  "requestData": {
+    "channelId": 4,
+    "type": 2,
+    "id": id,
     "keyword": keyword,
     "value": value,
     "delimiter": "",
@@ -33,30 +66,60 @@ def call_api(keyword, value):
   }
 }
     json_data = json.dumps(body)
-    print("json_data=", json_data)
-
     conn.request("POST", url, json_data, headers)
     response = conn.getresponse()
     rsbody = response.read()
-    print(response.status, rsbody)
+    jo = json.loads(rsbody)
+    print("call_api_upsert(): keyword=", keyword, "value=", value, "id=", id,
+        "status=", response.status, "resCode1=", jo["resCode1"], "resCode2=", jo["resCode2"], "detailMsg=", jo["detailMsg"])
 
-def process_csv_userdict(file_path):
-    noun_dict = { }
-    total = 0
-    print("process_csv() start: {} {}".format(file_path, datetime.today()))
-    with open(file_path) as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            title = row['title']
-            if (len(title) > 0):
-                cnoun_list = process_title(title)
-                for cnoun in cnoun_list:
-                    noun_dict[cnoun] = cnoun
-            total += 1
-#            if (total >= 100):
-#               break
-    return noun_dict
+def call_api_delete(id):
+    conn = http.client.HTTPConnection(API_HOST, API_PORT)
+    url = API_DICT_PATH + "/delete"
 
+    headers = {'Content-type': 'application/json'}
+    body = {
+  "chnl": "vcoloring",
+  "requestData": {
+    "channelId": 4,
+    "type": 2,
+    "id": id,
+    "userId": "1001291"
+  }
+}
+    json_data = json.dumps(body)
+    conn.request("POST", url, json_data, headers)
+    response = conn.getresponse()
+    rsbody = response.read()
+    jo = json.loads(rsbody)
+    print("call_api_delete(): keyword=", keyword, 
+        "status=", response.status, "resCode1=", jo["resCode1"], "resCode2=", jo["resCode2"], "detailMsg=", jo["detailMsg"])
+
+def insert_dict(keyword, value):
+    call_api_upsert(keyword, value, 0)
+
+def update_dict(keyword, value):
+    id = call_api_list(keyword)
+    if (id != 0):
+        call_api_upsert(keyword, value, id)
+    else:
+        print("insert_dict(): id not found")
+
+def upsert_dict(keyword, value):
+    id = call_api_list(keyword)
+    if (id != 0):
+        call_api_upsert(keyword, value, id)
+    else:
+        call_api_upsert(keyword, value, 0)
+
+def delete_dict(keyword):
+    id = call_api_list(keyword)
+    if (id != 0):
+        call_api_delete(id)
+    else:
+        print("delete_dict(): id not found")
+
+# 유사어 사전 생성 - 출력
 def process_csv_synonym(file_path):
     noun_dict = { }
     print("process_csv_synonym() start: {} {}".format(file_path, datetime.today()))
@@ -67,17 +130,40 @@ def process_csv_synonym(file_path):
             value = row['유의어']
             print(f"{keyword} => {value}")
 
-def process_csv_synonym2(file_path):
+# 유사어 사전 생성 - API 호출
+# TODO 이미 존재하는 경우 아래 에러 출력 -> update 처리
+# call_api_upsert(): keyword= Aminé value= Aminé,아미네,Amine id= 0 response.status= 200 resCode1= 999 resCode2= A001 detailMsg= 이미 등록된 검색어입니다.
+def create_csv_synonym(file_path):
     noun_dict = { }
-    print("process_csv_synonym_keyword() start: {} {}".format(file_path, datetime.today()))
+    print("create_csv_synonym() start: {} {}".format(file_path, datetime.today()))
     with open(file_path) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             keyword = row['검색어']
             value = row['유사어']
-            print(f"{keyword} => {value}")
+            insert_dict(keyword.strip(), value)
 
-#process_csv_synonym("rmc-ict-synonym-20211206.csv")
-#process_csv_synonym2("rmc-ict-synonym-create-202202.csv")
-call_api("aaa1", "aaa1")
+# 유사어 사전 수정 - API 호출
+# TODO id not found 에러 발생시 아래 에러 출력 - 0건 조회 경우, 2건 이상 조회 경우 반환된 list에서 keyword 값을 다시 찾아 봐야함
+# insert_dict(): id not found
+def update_csv_synonym(file_path):
+    noun_dict = { }
+    print("update_csv_synonym() start: {} {}".format(file_path, datetime.today()))
+    with open(file_path) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            keyword = row['검색어']
+            value = row['유사어']
+            update_dict(keyword.strip(), value)
+
+### 테스트
+#upsert_dict("aaa1", "aaa1")
+#delete_dict("aaa1")
+
+### Main
+#process_csv_synonym("input/rmc-ict-synonym-20211206.csv")
+
+#create_csv_synonym("input/rmc-ict-synonym-202202-create.csv")
+#update_csv_synonym("input/rmc-ict-synonym-202202-create2.csv")
+#update_csv_synonym("input/rmc-ict-synonym-202202-update.csv")
 
